c5d65d8921d42956e6b571f9a75a4b17dcf6ed9d
magyarakoos <magyar.kende.22f@gmail.com>
2025-12-30 16:24:35 +0100

    Add strong typing to app state

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,1 +1,7 @@
-export let box = $state({ stage: 1, qrCodeLink: null, imageData: null });
+export let box: BoxState = $state({ stage: 1, freeze: false, imageData: null });
+
+type BoxState = {
+    stage: number,
+    freeze: boolean,
+    imageData: ImageData | null,
+};
