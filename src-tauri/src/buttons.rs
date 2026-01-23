6f780da401a825b32951e4bf447cc4d09e8e0c21
magyarakoos <magyar.kende.22f@gmail.com>
2026-01-23 18:27:23 +0100

    Async interrupts set

--- src-tauri/src/buttons.rs
+++ src-tauri/src/buttons.rs
@@ -4,9 +4,24 @@ use std::{thread, time::Duration};
 
 pub fn run_buttons() {
     let gpio = Gpio::new().unwrap();
-    let pin = gpio.get(3).unwrap().into_input_pullup();
-    loop {
-        info!("Pin is_low: {}", pin.is_low());
-        thread::sleep(Duration::from_secs(1));
-    }
+    let mut button1 = gpio.get(2).unwrap().into_input_pullup();
+    let mut button2 = gpio.get(3).unwrap().into_input_pullup();
+    button1
+        .set_async_interrupt(
+            rppal::gpio::Trigger::FallingEdge,
+            Some(Duration::from_millis(100)),
+            |_| {
+                info!("Button1 pressed");
+            },
+        )
+        .unwrap();
+    button2
+        .set_async_interrupt(
+            rppal::gpio::Trigger::FallingEdge,
+            Some(Duration::from_millis(100)),
+            |_| {
+                info!("Button2 pressed");
+            },
+        )
+        .unwrap();
 }
