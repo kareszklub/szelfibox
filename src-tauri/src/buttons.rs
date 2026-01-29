31c569a12c27e272652862ce13228ed4df0cb36f
magyarakoos <magyar.kende.22f@gmail.com>
2026-01-29 10:26:24 +0100

    Make the GPIO interface check if it's running on a Pi instead of crashing

--- src-tauri/src/buttons.rs
+++ src-tauri/src/buttons.rs
@@ -1,10 +1,16 @@
-use log::info;
+use log::{error, info};
 use rppal::gpio::Gpio;
 use std::{thread, time::Duration};
 use tauri::Emitter;
 
 pub fn run_buttons(app: tauri::AppHandle) {
-    let gpio = Gpio::new().unwrap();
+    let gpio = match Gpio::new() {
+        Ok(gpio) => gpio,
+        Err(_) => {
+            error!("GPIO output not found, buttons will not be available.");
+            return;
+        }
+    };
     let mut button1 = gpio.get(2).unwrap().into_input_pullup();
     let mut button2 = gpio.get(3).unwrap().into_input_pullup();
 
@@ -15,7 +21,7 @@ pub fn run_buttons(app: tauri::AppHandle) {
                 rppal::gpio::Trigger::FallingEdge,
                 Some(Duration::from_millis(100)),
                 move |_| {
-                    app.emit::<i32>("button", 1);
+                    app.emit::<i32>("button", 1).unwrap();
                     info!("Button1 pressed");
                 },
             )
@@ -26,11 +32,12 @@ pub fn run_buttons(app: tauri::AppHandle) {
             rppal::gpio::Trigger::FallingEdge,
             Some(Duration::from_millis(100)),
             move |_| {
-                app.emit::<i32>("button", 2);
+                app.emit::<i32>("button", 2).unwrap();
                 info!("Button2 pressed");
             },
         )
         .unwrap();
+
     loop {
         thread::sleep(Duration::from_secs(1));
     }
