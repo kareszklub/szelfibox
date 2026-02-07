03e17af5bff18d2df2225c724b157cd4bc0c8f0f
magyarakoos <magyar.kende.22f@gmail.com>
2026-02-07 16:30:30 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run `./build.sh rebuild` if you want to rebuild it
+
+MODULE="v4l2loopback"
+
+if ! lsmod | grep -q "^${MODULE}"; then
+    echo "${MODULE} not found. Loading it now..."
+    sudo modprobe "$MODULE" exclusive_caps=1 card_label="SzelfiBox-Virtual-Cam"
+else
+    echo "${MODULE} is already loaded."
+fi
+
+if [ "$1" == "rebuild" ]; then
+    echo "Rebuilding now..."
+    npm run tauri build -- --no-bundle
+else
+    echo "No rebuilding, using latest build."
+fi
+
+cd src-tauri
+
+./target/release/szelfibox
